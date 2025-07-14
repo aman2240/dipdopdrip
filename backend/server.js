c10736ee01d8ea15a0e2e73e20e9ee7809f26ca7
aman2240 <aman7667132240@gmail.com>
2025-07-14 22:20:25 +0530

    originfull update

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const razorpayRoutes = require("./routes/razorpayRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "https://dipdopdrip-wor8.vercel.app" ,
+  origin: "https://www.dipdopdrip.in" ,
   credentials: true
 }));
 dotenv.config();
